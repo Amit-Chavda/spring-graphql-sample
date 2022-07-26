38fa9846b9adae539dc6aff63bc171c922e8d7f8
Amit Chavda <amitchavda9879@gmail.com>
2022-07-26 19:25:45 +0530

    Mutation added: save and removeById

--- src/main/java/com/graphql/sample/controller/UserController.java
+++ src/main/java/com/graphql/sample/controller/UserController.java
@@ -6,7 +6,6 @@ import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -14,30 +13,31 @@ import java.util.List;
 @Controller
 public class UserController {
 
-    private UserService userService;
+	private UserService userService;
 
 
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
+	public UserController(UserService userService) {
+		this.userService = userService;
+	}
 
-    @QueryMapping(name = "userById")
-    public UserDto findUserById(@Argument Long id) {
-        return userService.findById(id);
-    }
+	@QueryMapping(name = "userById")
+	public UserDto findUserById(@Argument Long id) {
+		return userService.findById(id);
+	}
 
-    public String removeUserById(@Argument Long id) {
-        return userService.removeById(id);
-    }
+	@MutationMapping(name = "removeById")
+	public String removeUserById(@Argument Long id) {
+		return userService.removeById(id);
+	}
 
+	@MutationMapping(name = "save")
+	public UserDto save(@Argument(name = "userInput") UserDto user) {
+		return  userService.save(user);
+	}
 
-    public UserDto save(@Argument UserDto user) {
-        return userService.save(user);
-    }
-
-    @QueryMapping(name = "users")
-    public List<UserDto> findAll() {
-        return userService.findAll();
-    }
+	@QueryMapping(name = "users")
+	public List<UserDto> findAll() {
+		return userService.findAll();
+	}
 
 }
